61b484ab355e036068592e7e3a27b6bddcc82465
Matthew <kindtvorter@gmail.com>
2025-09-01 00:43:23 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,24 @@
  *
  * @return {number}
  */
+
+const DAILY_COST = 40;
+const THREE_DAYS_DISCOUNT = 20;
+const SEVEN_DAYS_DISCOUNT = 50;
+const THRESHOLD_THREE_DAYS = 3;
+const THRESHOLD_SEVEN_DAYS = 7;
+
 function calculateRentalCost(days) {
   // write code here
+  const cost = DAILY_COST * days;
+
+  if (days >= THRESHOLD_SEVEN_DAYS) {
+    return cost - SEVEN_DAYS_DISCOUNT;
+  } else if (days >= THRESHOLD_THREE_DAYS) {
+    return cost - THREE_DAYS_DISCOUNT;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
